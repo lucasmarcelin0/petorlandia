ef58d0e16ee72daec560756698412942e5f1a149
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-06-23 12:12:21 -0300

    add config class for heroku deployment

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///petorlandia.db")
-
-# Corrigir Heroku PostgreSQL URL (caso necessário)
-if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///petorlandia.db")
+    
+    # Corrigir Heroku PostgreSQL URL (caso necessário)
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "chave-secreta-padrao")
