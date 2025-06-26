21be2c4b4360793fd9cf15c453d6e1512c32f1d3
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-06-25 22:23:38 -0300

    add s3 aws

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
-from extensions import db
+try:
+    from extensions import db
+except ImportError:
+    from .extensions import db
+
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
