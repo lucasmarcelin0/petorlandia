c2441f732dd947ccfeb72ec4286354b6bc233559
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-07-28 06:28:00 -0700

    Add Flask-Talisman security headers

--- extensions.py
+++ extensions.py
@@ -4,6 +4,7 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_session import Session
 from flask_babel import Babel
+from flask_talisman import Talisman
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,3 +12,4 @@ mail = Mail()
 login = LoginManager()
 session = Session()
 babel = Babel()
+talisman = Talisman()
