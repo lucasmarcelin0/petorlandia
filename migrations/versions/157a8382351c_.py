61302a884a265178bb5f4654fdedcfc383cdde8c
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-07-29 09:03:25 -0300

    animal and tutor default images

---
+++ migrations/versions/157a8382351c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 157a8382351c
+Revises: 01fb1a503d86, def45678
+Create Date: 2025-07-29 08:07:19.899604
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '157a8382351c'
+down_revision = ('01fb1a503d86', 'def45678')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
