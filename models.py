b14e290ecae3996aa475fb6461891430b5e90a17
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-08-29 08:29:13 -0700

    Make scheduled appointments row clickable

--- models.py
+++ models.py
@@ -636,6 +636,7 @@ class Appointment(db.Model):
     veterinario_id = db.Column(db.Integer, db.ForeignKey('veterinario.id'), nullable=False)
     scheduled_at = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(20), nullable=False, default='scheduled')
+    notes = db.Column(db.Text, nullable=True)
     consulta_id = db.Column(db.Integer, db.ForeignKey('consulta.id'), nullable=True)
     clinica_id = db.Column(db.Integer, db.ForeignKey('clinica.id'), nullable=True)
 
