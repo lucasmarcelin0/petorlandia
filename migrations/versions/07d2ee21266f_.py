d53d3aca65c1e202cc7a56bd87e78168e217f524
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-08-29 19:09:44 -0300

    fix alembic marker + merge migration

---
+++ migrations/versions/07d2ee21266f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 07d2ee21266f
+Revises: 9d3d3cdb1de4, def340caa273
+Create Date: 2025-08-29 19:03:05.850064
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '07d2ee21266f'
+down_revision = ('9d3d3cdb1de4', 'def340caa273')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/def340caa273_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: def340caa273
+Revises: 2b3b6d9f4c1a, 31b09172852f
+Create Date: 2025-08-29 18:57:56.224160
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'def340caa273'
+down_revision = ('2b3b6d9f4c1a', '31b09172852f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
