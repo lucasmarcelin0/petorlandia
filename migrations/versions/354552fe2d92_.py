3aad707e68853e05b849f9c37c3229a3c50ed33e
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-08-31 00:02:25 -0300

    clinica, clinica layout

---
+++ migrations/versions/354552fe2d92_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 354552fe2d92
+Revises: c1d9e6e54a3b, cd4535f122b0
+Create Date: 2025-08-30 23:30:23.054564
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '354552fe2d92'
+down_revision = ('c1d9e6e54a3b', 'cd4535f122b0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
