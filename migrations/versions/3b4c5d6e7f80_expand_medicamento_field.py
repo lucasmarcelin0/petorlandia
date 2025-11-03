5c174f4503eb33941d87060d3df3549c9b508e12
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2025-11-03 10:08:24 -0300

    Allow longer medicamento descriptions

---
+++ migrations/versions/3b4c5d6e7f80_expand_medicamento_field.py
@@ -0,0 +1,36 @@
+"""expand medicamento field length
+
+Revision ID: 3b4c5d6e7f80
+Revises: c4c9d1e2f3a4
+Create Date: 2025-11-03 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3b4c5d6e7f80'
+down_revision = 'c4c9d1e2f3a4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        'prescricao',
+        'medicamento',
+        existing_type=sa.String(length=100),
+        type_=sa.Text(),
+        existing_nullable=False,
+    )
+
+
+def downgrade():
+    op.alter_column(
+        'prescricao',
+        'medicamento',
+        existing_type=sa.Text(),
+        type_=sa.String(length=100),
+        existing_nullable=False,
+    )
