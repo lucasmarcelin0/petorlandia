6d3267fd06698ad71612ed0eb8b157f4079c3eb3
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2026-01-27 18:29:53 -0300

    claude trying to fix consulta from different clinics and time problem

---
+++ migrations/versions/55f047ac5443_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 55f047ac5443
+Revises: 7fa1e39ece61, 9dc09728e163
+Create Date: 2026-01-26 15:13:48.558746
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '55f047ac5443'
+down_revision = ('7fa1e39ece61', '9dc09728e163')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/9dc09728e163_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9dc09728e163
+Revises: 9b2f6fd0f6b2, bcafa8e99f34
+Create Date: 2026-01-26 15:04:06.444226
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9dc09728e163'
+down_revision = ('9b2f6fd0f6b2', 'bcafa8e99f34')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
