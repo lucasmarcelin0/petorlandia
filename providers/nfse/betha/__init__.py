b21765f9840dc07a59c32b05447cc24f528ab3c6
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2026-01-30 22:40:51 -0300

    copilot installing nfse necessary things

--- providers/nfse/betha/__init__.py
+++ providers/nfse/betha/__init__.py
@@ -1,1 +1,5 @@
 """Betha NFSe provider helpers."""
+from .xml_builder import build_lote_xml, build_rps_xml
+from .xml_signer import sign_betha_xml
+
+__all__ = ["build_lote_xml", "build_rps_xml", "sign_betha_xml"]
