0420fdc564b61952f834c56b6ff5488355290b76
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2026-01-30 22:14:17 -0300

    copilot failing

---
+++ migrations/versions/4897e091d7c7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4897e091d7c7
+Revises: 
+Create Date: 2026-01-30 22:04:47.029481
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4897e091d7c7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
