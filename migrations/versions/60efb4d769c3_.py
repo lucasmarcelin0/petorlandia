bc6ecb2f9d864b76f4cea8784771ea6851346b66
lucasmarcelin0 <66127687+lucasmarcelin0@users.noreply.github.com>
2026-05-04 21:45:26 -0300

    hey

---
+++ migrations/versions/60efb4d769c3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 60efb4d769c3
+Revises: 4a8ee3b7c3c8, a6d9e2c4b7f1
+Create Date: 2026-05-04 21:43:52.646831
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '60efb4d769c3'
+down_revision = ('4a8ee3b7c3c8', 'a6d9e2c4b7f1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
